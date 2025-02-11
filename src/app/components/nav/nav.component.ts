419c89a934df5a72a2aa2d5efe8651784c95ccce
Daniel Luna <daniel.00000853754@unicap.br>
2025-02-11 10:00:24 -0300

    first-components

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [ RouterLink, NgbCollapseModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-
+  isMenuCollapsed = true;
 }
